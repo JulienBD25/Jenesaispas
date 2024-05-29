7014df7e9001cb64caa6b62ad7dd104bcc5f6bb0
Maxlepalmier <maxime.delplace@edu.ece.fr>
2024-05-29 14:31:38 +0200

    Ajout pages

--- Votre_Compte_Client_Se_Connecter.php
+++ Votre_Compte_Client_Se_Connecter.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "medicare";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -16,7 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Check if user exists
     $sql = "SELECT * FROM clients WHERE Email = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $email);
@@ -24,10 +21,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
-        // User exists, redirect to existing user page
+
         header("Location: Accueil_Client.html");
     } else {
-        // User does not exist, redirect to new account creation page
+
         header("Location: Votre_Compte_Client_Creer_Compte.html");
     }
 
