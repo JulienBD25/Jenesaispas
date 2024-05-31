b97e26e7c012dd2d0c922b632d76bad830878820
gibertjules <133090934+gibertjules@users.noreply.github.com>
2024-05-31 15:51:12 +0200

    commit

--- I.S.T._Client.php
+++ I.S.T._Client.php
@@ -29,7 +29,15 @@ foreach ($xml->personnels_sante as $personnel) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nos Médecins Spécialistes en I.S.T</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="icon" href="Images/Logo_icone.ico" type="image/x-icon">
+
+    <!-- Bibliothèque jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
+    <!-- Dernier JavaScript compilé -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <style>
 
         .container {
